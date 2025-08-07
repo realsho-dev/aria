f506d685d269d65b234f3d64f21a5e4c1943253f
sho <168981721+realsho-dev@users.noreply.github.com>
2025-08-07 11:16:39 +0530

    Update status.py

--- bot/status.py
+++ bot/status.py
@@ -1,32 +1,15 @@
 import discord
-from discord.ext import tasks, commands
-import random
+from discord.ext import commands
 
 class Status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.statuses = [
-            "just here, making your day better",
-            "lowkey the smartest bot around",
-            "flirting with code and you",
-            "serving sass and answers all day",
-            "too cool to be just a bot",
-            "spicing up your chat sessions",
-            "your virtual vibe master",
-            "chatting, charming, conquering",
-            "here to steal your attention",
-            "no filter, all fire responses"
-        ]
-        self.change_status.start()
+        self.bot.loop.create_task(self.set_status())
 
-    @tasks.loop(minutes=5)
-    async def change_status(self):
-        new_status = random.choice(self.statuses)
-        await self.bot.change_presence(activity=discord.Game(name=new_status))
-
-    @change_status.before_loop
-    async def before_change_status(self):
+    async def set_status(self):
         await self.bot.wait_until_ready()
+        activity = discord.Activity(type=discord.ActivityType.listening, name="your commands.")
+        await self.bot.change_presence(activity=activity)
 
 async def setup(bot):
     await bot.add_cog(Status(bot))
