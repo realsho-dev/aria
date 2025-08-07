dbca84a53b0632ac0e78e82185e577919fa37744
sho <168981721+realsho-dev@users.noreply.github.com>
2025-08-07 11:13:37 +0530

    Update help.py

--- utility/help.py
+++ utility/help.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 async def help_command(ctx):
     msg = (
         "**Available commands:**\n"
-        "**ban**, **clearwarns**, **help** (shows this message), **hide**, **kick**, **lock**, **nuke**,\n"
+        "**ban**, **clearwarns**, **help**, **hide**, **kick**, **lock**, **nuke**,\n"
         "**purge**, **role**, **si**, **steal**, **timeout**, **ui**, **unhide**, **unlock**,\n"
         "**warn**, **warnings**"
     )
