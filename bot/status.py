2c20add8685629dfc3b15de66d85876fa828c73d
sho <168981721+realsho-dev@users.noreply.github.com>
2025-08-07 11:21:26 +0530

    Update status.py

--- bot/status.py
+++ bot/status.py
@@ -1,15 +1,14 @@
 import discord
-from discord.ext import commands
+from discord.ext import tasks, commands
 
 class Status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.bot.loop.create_task(self.set_status())
 
-    async def set_status(self):
-        await self.bot.wait_until_ready()
-        activity = discord.Activity(type=discord.ActivityType.listening, name="your commands.")
-        await self.bot.change_presence(activity=activity)
+    @commands.Cog.listener()
+    async def on_ready(self):
+        # Set the bot status once when bot is ready
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="your commands"))
 
 async def setup(bot):
     await bot.add_cog(Status(bot))
